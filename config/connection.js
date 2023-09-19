d87e7aecd6aad6870e8abb364f8d37774a31eb14
Dunadunn <alxrodrig7@gmail.com>
2023-09-18 20:45:29 -0700

    added config, server, controllers folder for placeholders

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,14 @@
-// config/connection.js
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
-  port: 3306
-});
-
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PW,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  }
+);
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,7 +1,39 @@
+const express = require('express');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
-require('dotenv').config();
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const path = require('path');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// ... your other server setup code ...
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Middleware setup
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Use static public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+// Use routes
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening on port:', PORT));
