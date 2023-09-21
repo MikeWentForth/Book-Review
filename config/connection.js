b1482b0198ba5b1d5cc38a59f8a8ddba51cf9e51
MikeWentForth <themikewallace@gmail.com>
2023-09-21 11:08:09 -0700

    began work on handlebars and bootstrap

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,8 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PW,
   {
-    host: 'localhost',
+    //host: 'localhost',
+    host: '127.0.0.1',  // 'localhost' not working on some Mac systems. Use 127.0.0.1.
     dialect: 'mysql',
     port: 3306
   }
