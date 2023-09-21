46c8f26d12e65bc6039767241c6a1dd97a5ab905
raphael.sanjuan13@gmail.com <raphael.sanjuan13@gmail.com>
2023-09-20 19:08:03 -0700

    fixed association and models

--- models/User.js
+++ models/User.js
@@ -16,6 +16,14 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false
     },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -23,6 +31,7 @@ User.init(
         len: [8]
       }
     }
+
   },
   {
     hooks: {
