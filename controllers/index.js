8d9763361af9ead39c7424e6a1f6b7250e9240af
MikeWentForth <themikewallace@gmail.com>
2023-09-24 19:20:56 -0700

    more front end updated including button functionality

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render("index",{layout: "main"});
+    res.render("index",{
+        layout: "main",
+        loggedIn: req.session.logged_in === true
+    });
     //res.send('Placeholder route.');
 });
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,74 @@
-/*
-Need to add event listeners for:
-IDs need to be added for high, recent, low
 
-    Login button
-    Add book button
-    Search-submit button
-    highest rating button
-    lowest rating button
-    recent button
+// Check whether the intended button is present. If so, add the listener.
 
-Need to add related functions for those listeners....
+let e;
 
-*/
+// LOGIN
+e = document.getElementById("login");
+if (e) {
+    e.addEventListener("click", function() {
+        alert("Login was clicked.")
+    });
+}
+
+// LOGOUT
+e = document.getElementById("logout");
+if (e) {
+    e.addEventListener("click", function() {
+        alert("Logout was clicked.")
+    });
+}
+
+// SIGNUP
+e = document.getElementById("signUp");
+if (e) {
+    e.addEventListener("click", function() {
+        alert("Signup was clicked.")
+    });
+}
+
+// ADD BOOK
+e = document.getElementById("addBook");
+if (e) {
+    e.addEventListener("click", function() {
+        alert("add book was clicked.")
+    });
+}
+
+// SEARCH BUTTON CLICKED
+e = document.getElementById("search");
+if (e) {
+    e.addEventListener("click", function() {
+        alert("search was clicked.")
+    });
+}
+
+
+// SORT BY MOST RECENT
+e = document.getElementById("mostRecent");
+if (e) {
+    e.addEventListener("click", function() {
+        alert("mosdt recent was clicked.")
+    });
+}
+
+
+// SORT BY HIGHEST RATED
+e = document.getElementById("highestRated");
+if (e) {
+    e.addEventListener("click", function() {
+        alert("highest rated was clicked.")
+    });
+}
+
+
+// SORT BY LOWEST RATED
+
+e = document.getElementById("lowestRated");
+if (e) {
+    e.addEventListener("click", function() {
+        alert("lowest rated was clicked.")
+    });
+}
+
+
