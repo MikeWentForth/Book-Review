44bae1a2311c0cccb1f77b35388c5c44e12525f9
raphael.sanjuan13@gmail.com <raphael.sanjuan13@gmail.com>
2023-09-25 19:44:11 -0700

    added links on express-async-handler implmentation

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,4 +1,8 @@
 module.exports = (err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ error: 'Internal Server Error! Unable to process your request at this time' });
+    //https://expressjs.com/en/guide/error-handling.html
 };
+
+// express async handlers
+// https://www.npmjs.com/package/express-async-handler
